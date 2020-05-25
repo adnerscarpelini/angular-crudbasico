46b20d7ae752d3fec192fbe34ff8d444b5de0b37
Adner Scarpelini <49483327+adnerscarpelini@users.noreply.github.com>
2020-05-25 13:30:58 -0300

    Criação Componente Navegação

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,18 +10,25 @@ import { HeaderComponent } from './components/template/header/header.component';
 //Importação dos componentes do AngularMaterial
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
