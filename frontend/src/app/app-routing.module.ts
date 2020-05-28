b772477f22baf1485927e2cb89b8bbd750f282ff
Adner Scarpelini <49483327+adnerscarpelini@users.noreply.github.com>
2020-05-28 18:36:48 -0300

    Configuração das Rotas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
-
-const routes: Routes = [];
+// ADNER: Aqui são configurados os links da aplicação.
+// Cada link aponta para  um determinado componente/view
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "products",
+    component: ProductCrudComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
