2536d9c66f8de44ad3623aca89c421e620b56b36
Adner Scarpelini <49483327+adnerscarpelini@users.noreply.github.com>
2020-06-08 18:53:58 -0300

    Criação da Table de Produto

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -10,17 +10,18 @@ import { Component, OnInit } from '@angular/core';
 export class ProductReadComponent implements OnInit {
 
   products: Product[];
+  displayedColumns = ['id', 'name', 'price', 'action']; //Colunas para exibir na tabela
 
   constructor(private productService: ProductService) { }
 
   //ASSIM QUE INICIA O COMPONENTE CONSULTA OS PRODUTOS
   ngOnInit(): void {
-    
+
     //Chama o método no serviço de produtos (product.service.ts)
     //Assim que a API dá a resposta carrega a lista
     this.productService.read().subscribe(products => {
       this.products = products;
-      console.log(products);
+      //console.log(products);
     })
 
   }
